17e46808f50451d4cbd7b4ab146553275b48c48a
Luca <lucaciabo@hotmail.it>
2020-03-17 12:36:56 +0100

    Added new alignment function to align all wrt the first cycle

--- DevelopmentScripts/PoseAlignment.py
+++ DevelopmentScripts/PoseAlignment.py
@@ -18,15 +18,18 @@ def getDtwPath(frameSequence1, frameSequence2, weights=None):
 
 def findNext(value, paths, pathsIndex):
     """
-
-    :param value:
-    :param paths:
-    :param pathsIndex:
+    Recursive function to find all the frames that belongs to the same pose in different cycles.
+    e.g. 0|0 is connected with 0|1; so the function search all the frames in other cycles where there is 0|1. So if
+    there is a couple 0|1,0|2 then 0|2 will be added the the list of the same poses.
+    :param value: element we are searching e.g. 0|1 -> we search for elements that start for 0|1
+    :param paths:matrix with all the paths
+    :param pathsIndex: index in the paths matrix (tells what cycle in the paths we are reffering to
     :return:
     """
     i = 0
     j = 0
     tmp = []
+    #the while check if we should go on searching for element in the list
     while paths[pathsIndex][i][j][0] <= int(value.split('|')[0]) and i < len(paths[pathsIndex]) - 1:
         alreadySeen = False
         for j in range(0, len(paths[pathsIndex][i])):
@@ -41,7 +44,7 @@ def findNext(value, paths, pathsIndex):
     return tmp
 
 
-def align1frame1pose(keyPoints, mins, weights=None):
+def align1frame1pose_tmp(keyPoints, mins, weights=None):
     """
     Given the local mins of the cycles, align the cycles of an exercise execution
     :param keyPoints: numpy array of keypoints (#frames,25,2)
@@ -93,7 +96,6 @@ def align1frame1poseFirstCycle(keyPoints, mins, weights=None):
         frameSequence1 = keyPoints[mins[0]:mins[1]]
         frameSequence2 = keyPoints[mins[i + 1]: mins[i + 2]]
         path = getDtwPath(frameSequence1, frameSequence2, weights)
-
         unifiedPath = []
         tmp = [path[0]]
         for j in range(1, len(path)):
@@ -110,7 +112,32 @@ def align1frame1poseFirstCycle(keyPoints, mins, weights=None):
         pose = []
         for j in range(0, len(paths[0][i])):
             pose += [str(paths[0][i][0][0]) + '|0']
-            pose += findNext(str(paths[0][i][j][1]) + '|1', paths, 1)
+            pose += findNextFirstCycle(str(paths[0][i][j][1]) + '|0', paths, 1)
         pose = list(dict.fromkeys(pose))
         poseMatrix.append(pose)
     return poseMatrix
+
+def findNextFirstCycle(value, paths, pathsIndex):
+    """
+    Recursive function to find all the frames that belongs to the same pose in different cycles.
+    :param value: element we are searching e.g. 0|1 -> we search for elements that start for 0|1
+    :param paths:matrix with all the paths
+    :param pathsIndex: index in the paths matrix (tells what cycle in the paths we are reffering to
+    :return:
+    """
+    i = 0
+    j = 0
+    tmp = []
+    #the while check if we should go on searching for element in the list
+    while paths[pathsIndex][i][j][0] <= int(value.split('|')[0]) and i < len(paths[pathsIndex]) - 1:
+        alreadySeen = False
+        for j in range(0, len(paths[pathsIndex][i])):
+            if int(value.split('|')[0]) == paths[pathsIndex][i][j][0]:
+                if alreadySeen is False:
+                    tmp += [str(paths[pathsIndex][i][0][0]) + '|' + str(pathsIndex)]
+                    alreadySeen = True
+                if pathsIndex < len(paths) - 1:
+                    tmp += findNextFirstCycle(value, paths, pathsIndex + 1)
+        i += 1
+        j = 0
+    return tmp
